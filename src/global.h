bcd15ce4e8c832761396420b975a417d0a424b04
agarciaruiz <82825274+agarciaruiz@users.noreply.github.com>
2022-10-12 12:54:30 +0200

    main header refactor

--- src/global.h
+++ src/global.h
@@ -4,4 +4,6 @@
 #define SCR_WIDTH 800
 #define SCR_HEIGHT 450
 
+enum Screens { NONE, LOGO, TITLE, OPTIONS, GAMEPLAY, ENDING };
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
-#include "raylib.h"
-#include "paratrooper.hpp"
+#include "main.hpp"
 
 //----------------------------------------------------------------------------------
 // Main entry point
@@ -8,6 +7,13 @@ int main(void)
 {
     // Initialization
     //---------------------------------------------------------
+    currentScreen = Screens::LOGO;
+    transAlpha = 0.0f;
+    onTransition = false;
+    transFadeOut = false;
+    transFromScreen = -1;
+    transToScreen = Screens::NONE;
+
     InitWindow(SCR_WIDTH, SCR_HEIGHT, "Paratrooper - Alberto Garcia");
 
     InitAudioDevice();      // Initialize audio device
@@ -22,7 +28,7 @@ int main(void)
     currentScreen = LOGO;
 
     //InitLogoScreen();
-    logoScreen->Init();
+    logoScreen.Init();
 
     SetTargetFPS(60);       // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -38,21 +44,16 @@ int main(void)
     // Unload current screen data before closing
     switch (currentScreen)
     {
-        case LOGO: logoScreen->Unload(); break;
-        case TITLE: titleScreen->Unload(); break;
-        case OPTIONS: optionsScreen->Unload(); break;
-        case GAMEPLAY: gameScreen->Unload(); break;
-        case ENDING: endingScreen->Unload(); break;
+        case LOGO: logoScreen.Unload(); break;
+        case TITLE: titleScreen.Unload(); break;
+        case OPTIONS: optionsScreen.Unload(); break;
+        case GAMEPLAY: gameScreen.Unload(); break;
+        case ENDING: endingScreen.Unload(); break;
         default: break;
     }
 
     // Unload global data loaded
     UnloadMusicStream(music);
-    delete logoScreen;
-    delete titleScreen;
-    delete optionsScreen;
-    delete gameScreen;
-    delete endingScreen;
 
     CloseAudioDevice();     // Close audio context
 
@@ -71,22 +72,22 @@ static void ChangeToScreen(Screens screen)
     // Unload current screen
     switch (currentScreen)
     {
-        case LOGO: logoScreen->Unload(); break;
-        case TITLE: titleScreen->Unload(); break;
-        case OPTIONS: optionsScreen->Unload(); break;
-        case GAMEPLAY: gameScreen->Unload(); break;
-        case ENDING: endingScreen->Unload(); break;
+        case LOGO: logoScreen.Unload(); break;
+        case TITLE: titleScreen.Unload(); break;
+        case OPTIONS: optionsScreen.Unload(); break;
+        case GAMEPLAY: gameScreen.Unload(); break;
+        case ENDING: endingScreen.Unload(); break;
         default: break;
     }
 
     // Init next screen
     switch (screen)
     {
-        case LOGO: logoScreen->Init(); break;
-        case TITLE: titleScreen->Init(); break;
-        case OPTIONS: optionsScreen->Init(); break;
-        case GAMEPLAY: gameScreen->Init(); break;
-        case ENDING: endingScreen->Init(); break;
+        case LOGO: logoScreen.Init(); break;
+        case TITLE: titleScreen.Init(); break;
+        case OPTIONS: optionsScreen.Init(); break;
+        case GAMEPLAY: gameScreen.Init(); break;
+        case ENDING: endingScreen.Init(); break;
         default: break;
     }
 
@@ -119,22 +120,22 @@ static void UpdateTransition(void)
             // Unload current screen
             switch (transFromScreen)
             {
-                case LOGO: logoScreen->Unload(); break;
-                case TITLE: titleScreen->Unload(); break;
-                case OPTIONS: optionsScreen->Unload(); break;
-                case GAMEPLAY: gameScreen->Unload(); break;
-                case ENDING: endingScreen->Unload(); break;
+                case LOGO: logoScreen.Unload(); break;
+                case TITLE: titleScreen.Unload(); break;
+                case OPTIONS: optionsScreen.Unload(); break;
+                case GAMEPLAY: gameScreen.Unload(); break;
+                case ENDING: endingScreen.Unload(); break;
                 default: break;
             }
 
             // Load next screen
             switch (transToScreen)
             {
-                case LOGO: logoScreen->Init(); break;
-                case TITLE: titleScreen->Init(); break;
-                case OPTIONS: optionsScreen->Init(); break;
-                case GAMEPLAY: gameScreen->Init(); break;
-                case ENDING: endingScreen->Init(); break;
+                case LOGO: logoScreen.Init(); break;
+                case TITLE: titleScreen.Init(); break;
+                case OPTIONS: optionsScreen.Init(); break;
+                case GAMEPLAY: gameScreen.Init(); break;
+                case ENDING: endingScreen.Init(); break;
                 default: break;
             }
 
@@ -178,39 +179,39 @@ static void UpdateDrawFrame()
         {
             case LOGO:
             {
-                logoScreen->Update();
+                logoScreen.Update();
 
-                if (logoScreen->FinishScreen()) TransitionToScreen(TITLE);
+                if (logoScreen.FinishScreen()) TransitionToScreen(TITLE);
 
             } break;
             case TITLE:
             {
-                titleScreen->Update();
+                titleScreen.Update();
 
-                if (titleScreen->FinishScreen() == 3) TransitionToScreen(OPTIONS);
-                else if (titleScreen->FinishScreen() == 4) TransitionToScreen(GAMEPLAY);
+                if (titleScreen.FinishScreen() == 3) TransitionToScreen(OPTIONS);
+                else if (titleScreen.FinishScreen() == 4) TransitionToScreen(GAMEPLAY);
 
             } break;
             case OPTIONS:
             {
-                optionsScreen->Update();
+                optionsScreen.Update();
 
-                if (optionsScreen->FinishScreen()) TransitionToScreen(TITLE);
+                if (optionsScreen.FinishScreen()) TransitionToScreen(TITLE);
 
             } break;
             case GAMEPLAY:
             {
-                gameScreen->Update();
+                gameScreen.Update();
 
-                if (gameScreen->FinishScreen() == 5) TransitionToScreen(ENDING);
+                if (gameScreen.FinishScreen() == 5) TransitionToScreen(ENDING);
 
             } break;
             case ENDING:
             {
-                endingScreen->Update();
+                endingScreen.Update();
 
-                if (endingScreen->FinishScreen() == 2) TransitionToScreen(TITLE);
-                else if (endingScreen->FinishScreen() == 3) TransitionToScreen(OPTIONS);
+                if (endingScreen.FinishScreen() == 2) TransitionToScreen(TITLE);
+                else if (endingScreen.FinishScreen() == 3) TransitionToScreen(OPTIONS);
 
             } break;
             default: break;
@@ -227,11 +228,11 @@ static void UpdateDrawFrame()
 
         switch(currentScreen)
         {
-            case LOGO: logoScreen->Draw(); break;
-            case TITLE: titleScreen->Draw(); break;
-            case OPTIONS: optionsScreen->Draw(); break;
-            case GAMEPLAY: gameScreen->Draw(); break;
-            case ENDING: endingScreen->Draw(); break;
+            case LOGO: logoScreen.Draw(); break;
+            case TITLE: titleScreen.Draw(); break;
+            case OPTIONS: optionsScreen.Draw(); break;
+            case GAMEPLAY: gameScreen.Draw(); break;
+            case ENDING: endingScreen.Draw(); break;
             default: break;
         }
 
